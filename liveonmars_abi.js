972a89d5d272c4e9bd04c254c634230c365fac87
thecloudguy-engineer <89355927+thecloudguy-engineer@users.noreply.github.com>
2022-04-28 20:14:29 -0500

    made a new dapp and added a tab
    This dapp only works on Rinkeby network

---
+++ liveonmars_abi.js
@@ -0,0 +1,54 @@
+var liveOnMarsABI = [
+    {
+        "inputs": [],
+        "name": "noCount",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "question",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "bool",
+                "name": "_answer",
+                "type": "bool"
+            }
+        ],
+        "name": "recordVote",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "yesCount",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+]

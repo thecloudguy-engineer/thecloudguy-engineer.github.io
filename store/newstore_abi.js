334c50c3cf4fa6c983d704395660440af4786fb4
thecloudguy <thecloudguy.engineer@gmail.com>
2022-05-01 20:52:28 -0500

    I thought the form input has also textcontent field. input doesnt have closing tag so it has only value field. async await works nicely in JS. It took sometime to work out the details

---
+++ store/newstore_abi.js
@@ -0,0 +1,28 @@
+var newStoreABI = [
+    {
+        "inputs": [],
+        "name": "favoriteNumber",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "_favoriteNumber",
+                "type": "uint256"
+            }
+        ],
+        "name": "store",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+]
